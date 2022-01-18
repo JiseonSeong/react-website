a36e5dbc9f13a8d20da0f9b96b5663b4f54afce6
JiseonSeong <jseong0987@gmail.com>
2022-01-18 15:02:37 -0500

    works but image is not showing

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElements";
 import {
   HeroContainer,
   HeroBg,
@@ -13,6 +14,10 @@ import {
 } from "./HeroElements";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
     <HeroContainer>
       <HeroBg>
@@ -24,11 +29,17 @@ const HeroSection = () => {
           Sign up for a new account today and receive $250 in credit towards our
           next payment.
         </HeroP>
-        {/* <HeroBtnWrapper>
-          <Button to="signup">
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
             Get start {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
-        </HeroBtnWrapper> */}
+        </HeroBtnWrapper>
       </HeroContent>
     </HeroContainer>
   );
