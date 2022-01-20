df9a3b49b5facde11e5b5355cdb85c76f0719b0a
JiseonSeong <jseong0987@gmail.com>
2022-01-19 20:16:44 -0500

    Changing Navbar background on scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBeer } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 import {
   Nav,
   NavbarContainer,
@@ -13,33 +14,48 @@ import {
 } from "./NavbarElements";
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">Dollar</NavLogo>
-          <MobileIcon onClick={toggle}>
-            <FaBeer />
-          </MobileIcon>
-          <NavMenu>
-            <NavItem>
-              <NavLinks to="about">About</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="discover">Discover</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="services">Services</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="signup">Sign Up</NavLinks>
-            </NavItem>
-          </NavMenu>
-          <NavBtn>
-            <NavBtnLink to="/signin">Sign in</NavBtnLink>
-          </NavBtn>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo to="/">Dollar</NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBeer />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="about">About</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="discover">Discover</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="signup">Sign Up</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/signin">Sign in</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
